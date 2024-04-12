348c53853bead55649a1424a873f994146b217e9
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-12 23:12:32 +0200

    basic component example

--- part01/src/App.jsx
+++ part01/src/App.jsx
@@ -1,7 +1,15 @@
 const App = () => {
+  const date = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(date, a + b);
+
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello World, it is {date.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   );
 };

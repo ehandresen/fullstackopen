73975d421c6c1ef2eacf878de3b9da8cc6dcc3c8
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-18 01:32:19 +0200

    refactoring components

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  };
 
-  const decreaseByOne = () => setCounter(counter - 1);
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  };
 
-  const setToZero = () => setCounter(0);
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
 
   return (
     <div>
@@ -19,12 +29,8 @@ const App = () => {
   );
 };
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 export default App;

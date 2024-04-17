e06f9c352b156bd11c6712b31913284b855a1be0
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-18 01:17:03 +0200

    passing state to child components

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -3,13 +3,28 @@ import { useState } from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0);
 
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const decreaseByOne = () => setCounter(counter - 1);
+
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(0)}>reset</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   );
+};
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 export default App;

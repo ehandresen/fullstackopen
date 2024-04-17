b94b0f3755f11d68222f8e78dd3833ca6e4bc8ec
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-17 14:31:12 +0200

    window.setInterval

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -1,26 +1,8 @@
-function App() {
-  const name = 'Peter';
-  const age = 10;
+const App = (props) => {
+  console.log(props);
+  const { counter } = props;
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={29} />
-      {/* <Hello name={name} age={age} /> */}
-    </div>
-  );
-}
-
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
+  return <div>{counter}</div>;
 };
 
 export default App;

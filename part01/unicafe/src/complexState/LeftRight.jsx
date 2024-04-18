8b2158850dcfe1eedac32959f9c07fa5d56c554f
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-18 15:09:06 +0200

    conditional rendering

--- part01/unicafe/src/complexState/LeftRight.jsx
+++ part01/unicafe/src/complexState/LeftRight.jsx
@@ -4,15 +4,18 @@ const LeftRight = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
     setLeft(left + 1);
     setAllClicks(allClicks.concat('L'));
+    setTotal(total + 1);
   };
 
   const handleRightClick = () => {
     setRight(right + 1);
     setAllClicks(allClicks.concat('R'));
+    setTotal(total + 1);
   };
 
   return (
@@ -21,9 +24,18 @@ const LeftRight = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>All clicks: {allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
+      <p>Total: {total}</p>
     </div>
   );
+};
+
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>Press a button</div>;
+  }
+
+  return <div>Button press history: {allClicks.join(' ')}</div>;
 };
 
 export default LeftRight;

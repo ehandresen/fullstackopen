1c98bf44969ad561917e80569268194597910e90
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-19 20:32:10 +0200

    exercise 2.2

--- part02/courseinfo/src/components/Content.jsx
+++ part02/courseinfo/src/components/Content.jsx
@@ -3,11 +3,21 @@ import Part from './Part';
 const Content = (props) => {
   const { parts } = props;
 
+  const total = parts.map((part) => part.exercises);
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p>
+        {'Total of ' +
+          total.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+          ) +
+          ' exercises'}
+      </p>
     </div>
   );
 };

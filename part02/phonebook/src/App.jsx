0b0ebb1098fc2a16b0258f778a0cbaa1de23719d
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-21 23:34:12 +0200

    started 2.6

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [person, setPerson] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form ></form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={person[0].name} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
     </div>
   );
-}
+};
 
 export default App;

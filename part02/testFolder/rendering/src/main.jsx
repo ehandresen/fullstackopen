746088942837788405a5b0800af37d856c2eccc0
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-24 15:24:26 +0200

    useEffect

--- part02/testFolder/rendering/src/main.jsx
+++ part02/testFolder/rendering/src/main.jsx
@@ -1,15 +1,4 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import axios from 'axios';
 
-axios
-  .get('http://localhost:3001/notes')
-  .then((response) => {
-    const notes = response.data;
-    console.log(notes);
-
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <App notes={notes} />
-    );
-  })
-  .catch((error) => console.log(error.message));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
